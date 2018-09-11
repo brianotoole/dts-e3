39d8d478b34fa13b80b465bb91afc6e5af617522
Brian O’Toole <brianzotoole@gmail.com>
2018-09-10 20:03:21 -0400

    accordion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /*********************************************************
   Main.js = main js entry point
 *********************************************************/
+import 'jquery';
+import 'popper.js';
 import 'bootstrap';
+
 
 import './main.scss';
