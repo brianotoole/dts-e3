f88a26e858314c589c8613e24dd93c1f9a5259d7
brianotoole <botoole518@gmail.com>
2018-09-12 19:27:03 -0400

    import jquery

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /*********************************************************
   Main.js = main js entry point
 *********************************************************/
-import 'jquery';
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
 import 'popper.js';
 import 'bootstrap';
 
 
 import './main.scss';
+
+$(window).scroll(function() {
+  var hT = $('#js-scroll-to').offset().top,
+      hH = $('#js-scroll-to').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+   console.log((hT-wH) , wS);
+  if (wS > (hT+hH-wH)){
+    alert('you have scrolled to the h1!');
+  }
+});
