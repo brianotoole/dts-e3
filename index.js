57b842ab23115f9c4ab0ebbae90ef018091c8884
brianotoole <botoole518@gmail.com>
2018-09-13 18:11:14 -0400

    change rgba bg color % on scroll w/ dynamic color

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ import "bootstrap";
 import "./main.scss";
 
 // Header fixed scroll
-jQuery("document").ready(function ($) {
+(function ($) {
   var header = $(".header");
-  var profile = $(".profile");
+  var profile = $(".page-body__header .profile__header");
   var profileTop = profile.offset().top;
   var profileHeight = profile.outerHeight();
   var visible = false;
@@ -32,4 +32,37 @@ jQuery("document").ready(function ($) {
 
   });
 
-});
+})(jQuery);
+
+
+(function ($) {
+  var target = $('.page-body__header');
+  var targetBgColor = target.css('backgroundColor'); // returns rgba value: rgb(234, 140, 42)
+  var targetHeight = target.outerHeight();
+
+  // we need value without precentage (4th match)
+  var convertToRGB = getRGBValue(targetBgColor);
+  var targetRGBString = 'rgba(' + convertToRGB.red + ',' + convertToRGB.green + ',' + convertToRGB.blue;
+  console.log(targetRGBString);
+  //console.log(convertToRGB.red, convertToRGB.green, convertToRGB.blue);
+
+  $(document).scroll(function (e) {
+    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    if (scrollPercent >= 0.1) {
+      //target.css('opacity', scrollPercent);
+      target.css('background-color', targetRGBString + ',' + scrollPercent);
+    }
+  });
+
+
+})(jQuery);
+
+function getRGBValue(str) {
+  var match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+  return match ? {
+    red: match[1],
+    green: match[2],
+    blue: match[3]
+  } : {};
+}
+
