b3a4aaf04e0bc6ff507e857b2a6a84ef0736d629
brianotoole <botoole518@gmail.com>
2018-09-19 18:40:17 -0400

    fix paths for fileloader imgs

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,101 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src//index.js'
+    app: "./src/index.js"
     //vendor: './src/js/vendor.js'
   },
   output: {
-    filename: 'app/js/[name].js',
-    publicPath: './'
+    path: path.resolve(__dirname, "app"),
+    filename: "./js/[name].js"
   },
   module: {
     rules: [
-      { // STYE LOADERS
+      {
+        // STYE LOADERS
         test: /\.(css|sass|scss)$/,
         use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          use: ["css-loader", "postcss-loader", "sass-loader"]
         })
       },
-      { // JS LOADERS
-        loader: 'babel-loader',
+      {
+        // JS LOADERS
+        loader: "babel-loader",
         query: {
-          presets: ['es2015']
+          presets: ["es2015"]
         },
         test: /\.js$/,
         exclude: /node_modules/
       },
-      { // URL LOADER, IMAGE FILES
-        test: /\.(jpe?g|png|svg)/,
-        loader: 'url-loader?limit=10000&name=dist/img/[name].[ext]', //if < 10 kb, base64 encode img to css
+      {
+        // FILE LOADER, IMAGES
+        test: /\.(png|jpg|gif|svg)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./img/",
+              publicPath: "../img/"
+            }
+          }
+        ]
       },
-      { // URL LOADER, FONTS
-        test: /\.(woff|woff2|eot|ttf)/,
-        loader: 'url-loader?limit=10000&name=dist/fonts/[name].[ext]', //font files to './dist/fonts/**.'
+      {
+        // FILE LOADER, FONTS
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./css/fonts/",
+              publicPath: "../"
+            }
+          }
+        ]
       },
-      { // HTML LOADER
+      {
+        // HTML LOADER
         test: /\.html$/,
         use: [
-          'html-loader',
+          "html-loader",
           {
-            loader: 'posthtml-loader',
+            loader: "posthtml-loader",
             options: {
               plugins: [
-                require('posthtml-include')({
-                  root: path.resolve(__dirname, 'src')
+                require("posthtml-include")({
+                  root: path.resolve(__dirname, "src")
                 })
               ]
             }
           }
         ]
-      },
-      /*{ // FILE-LOADER
-        test: /\.woff|woff2|eot|ttf|svg|jpe?g|png/,
-        loader: 'file-loader',
-        options: {
-          name: 'dist/img/[name].[ext]',
-          publicPath: function(url) {
-            return url.replace(/dist/, '..')
-          },
-        },
-      },*/
+      }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src/layouts/index.html') }),
-    new HtmlWebpackPlugin({ filename: 'org.html', template: path.resolve(__dirname, 'src/layouts/org.html') }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/layouts/index.html")
+    }),
+    new HtmlWebpackPlugin({
+      filename: "org.html",
+      template: path.resolve(__dirname, "src/layouts/org.html")
+    }),
     new ExtractTextPlugin({
-      filename: 'app/css/style.css',
-      allChunks: true,
+      filename: "css/style.css",
+      allChunks: true
     }),
     new BrowserSyncPlugin({
       notify: false, // hide the notification
-      host: 'localhost',
+      host: "localhost",
       port: 3000,
-      proxy: 'http://localhost/dts-3e',
-      files: [
-        '**/*.html'
-      ]
+      proxy: "http://localhost/dts-3e",
+      files: ["**/*.html"]
     })
-  ],
+  ]
 };
