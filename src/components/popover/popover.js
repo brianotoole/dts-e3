609ccd38a6a4cc651849d306dccc1ed93bd9e52c
brianotoole <botoole518@gmail.com>
2018-10-08 15:43:53 -0400

    update popover scrollTo

--- src/components/popover/popover.js
+++ src/components/popover/popover.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
       $("html, body").animate({ scrollTop: 0 }, "slow");
     } else {
       popover.removeClass("out").addClass("in");
-      $("html, body").animate({ scrollTop: popover.offset().top-10}, "slow");
+      $("html, body").animate({ scrollTop: popover.offset().top - popover.outerHeight()}, "slow");
     }
   });
 
